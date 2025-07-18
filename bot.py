5ac6489383555e86af290ea083f3687e983c8113
Strike-boy <ogabekmahmudzonov24@gmail.com>
2025-07-18 14:28:31 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from config.ру import BOT_TOKEN, ADMIN_ID
 from handlers import register_handlers
 import logging
+
+# 🔐 Конфигурация прямо здесь:
+BOT_TOKEN = "7661435901:AAFx8X7mY9wwW5FEeKofbLc9GddmX_tLlYk"
+ADMIN_ID = 1001788720
 
 # Включаем логирование
 logging.basicConfig(level=logging.INFO)
@@ -11,9 +14,9 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
 dp = Dispatcher(bot, storage=MemoryStorage())
 
-# Регистрируем все хендлеры
+# Подключение всех хендлеров
 register_handlers(dp)
 
 # Запуск бота
-if __name__ == '__main__':
+if name == 'main':
     executor.start_polling(dp, skip_updates=True)
