c5145be96d75e8a195e035b4cd9532ee6d77d681
Strike-boy <ogabekmahmudzonov24@gmail.com>
2025-07-18 15:25:56 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,23 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from handlers import register_handlers
 import logging
 
-# 🔐 Конфигурация прямо здесь:
-BOT_TOKEN = "7661435901:AAFx8X7mY9wwW5FEeKofbLc9GddmX_tLlYk"
+# 🔐 Конфигурация прямо тут
+BOT_TOKEN = "ТВОЙ_ТОКЕН_СЮДА"
 ADMIN_ID = 1001788720
+ACR_HOST = "identify-ap-southeast-1.acrcloud.com"
+ACR_ACCESS_KEY = "e48f0d7b2af6ccad4015b26d57d75903"
+ACR_SECRET_KEY = "WTWOUirBwcIPMJY6vOHEXVKilaMviC8doHQKGgaV"
 
-# Включаем логирование
+# 🧠 Инициализация
 logging.basicConfig(level=logging.INFO)
-
-# Инициализация
 bot = Bot(token=BOT_TOKEN, parse_mode="HTML")
 dp = Dispatcher(bot, storage=MemoryStorage())
 
-# Подключение всех хендлеров
+# 📦 Импорт регистраций
+from handlers import register_handlers
 register_handlers(dp)
 
-# Запуск бота
+# ▶️ Старт
 if name == 'main':
     executor.start_polling(dp, skip_updates=True)
