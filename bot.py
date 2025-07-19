f6fa13dd26f7b6c7ed6b227307c60e6f6cd87839
Strike-boy <ogabekmahmudzonov24@gmail.com>
2025-07-19 22:59:30 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -532,9 +532,9 @@ async def inline_query_handler(inline_query: types.InlineQuery):
 # === Flask сервер для Render / других хостингов ===
 from flask import Flask, request, abort
 
-RENDER_DOMAIN = os.environ.get("RENDER_EXTERNAL_URL", "https://tgreels.onrender.com").rstrip("/")
 WEBHOOK_PATH = f"/webhook/{TOKEN}"
-WEBHOOK_URL = f"('RENDER_EXTERNAL_URL', 'your-domain.com')}{WEBHOOK_PATH}"
+RENDER_DOMAIN = os.environ.get("RENDER_EXTERNAL_URL", "https://tgreels.onrender.com").rstrip("/")
+WEBHOOK_URL = f"{RENDER_DOMAIN}{WEBHOOK_PATH}"
 
 app = Flask(__name__)
 
