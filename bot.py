87ee566458d5a5b4926f9e8004ef26d073f2c49e
Strike-boy <ogabekmahmudzonov24@gmail.com>
2025-07-28 23:36:48 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -502,7 +502,6 @@ async def download_video(message: types.Message):
             'outtmpl': 'video.%(ext)s',
             'cookiefile': cookie_file,
             'format': 'bv*+ba/b',
-            'format': '18',
             'merge_output_format': 'mp4',
             'quiet': True,
             'no_warnings': True,
